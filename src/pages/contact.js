65eab57add270dff8dcd8a7c0c0bdb7122387ad4
hunterbecton <hunter@hunterbecton.com>
2019-03-14 17:35:50 -0400

    Added thanks page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,28 +12,28 @@ const Contact = () => (
             <div className='contact__form'>
             <h1>Contact</h1>
                 <div className='inner'>
-                        <form method='post' name='contact' data-netlify='true' netlify-honeypot='bot'>
-                        <input type="hidden" name="form-name" value="contact" />
-                            <div className='field__hidden'>
-                                <label>Don't fill this out, human</label>
-                                <input name='bot' />
-                            </div>
-                            <div className='field'>
-                                <label>Name</label>
-                                <input type='text' name='name'/>
-                            </div>
-                            <div className='field'>
-                                <label>Email</label>
-                                <input type='text' name='email'/>
-                            </div>
-                            <div className='field'>
-                                <label>Message</label>
-                                <textarea name='message' rows='6'></textarea>
-                            </div>
-                            <div className='submit'>
-                                <button type='submit' className='btn__med'>Send</button>
-                            </div>
-                        </form>
+                    <form method='post' name='contact' action='/thanks' data-netlify='true' netlify-honeypot='bot'>
+                    <input type="hidden" name="form-name" value="contact" />
+                        <div className='field__hidden'>
+                            <label>Don't fill this out, human</label>
+                            <input name='bot' />
+                        </div>
+                        <div className='field'>
+                            <label>Name</label>
+                            <input type='text' name='name'/>
+                        </div>
+                        <div className='field'>
+                            <label>Email</label>
+                            <input type='text' name='email'/>
+                        </div>
+                        <div className='field'>
+                            <label>Message</label>
+                            <textarea name='message' rows='6'></textarea>
+                        </div>
+                        <div className='submit'>
+                            <button type='submit' className='btn__med'>Send</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
