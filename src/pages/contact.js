14890cd5445ec07afb6a67e8d9b62734a5b54f8f
hunterbecton <hunter@hunterbecton.com>
2019-03-14 16:52:12 -0400

    Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,10 +12,11 @@ const Contact = () => (
             <div className='contact__form'>
             <h1>Contact</h1>
                 <div className='inner'>
-                        <form method='post' name='contact' action='/' data-netlify='true' netlify-honeypot='bot'>
+                        <form method='post' name='contact' data-netlify='true' netlify-honeypot='bot'>
+                        <input type="hidden" name="form-name" value="contact" />
                             <div className='field__hidden'>
                                 <label>Don't fill this out, human</label>
-                                <input type='hidden' name='bot' />
+                                <input name='bot' />
                             </div>
                             <div className='field'>
                                 <label>Name</label>
